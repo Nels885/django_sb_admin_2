76247eef0e6fb6356c77e25e14c3919822f5ba3b
Nels885 <voirin.lionel@gmail.com>
2019-04-23 15:48:46 +0200

    Add README.md file

--- csd_dashboard/urls.py
+++ csd_dashboard/urls.py
@@ -17,7 +17,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
+from dashboard import views
+
 urlpatterns = [
+    path('', views.index),
     path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ]
