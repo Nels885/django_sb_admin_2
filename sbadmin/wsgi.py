d853e1a1a92c3a04e6a4c312c6d3b527e92ab56b
Nels885 <voirin.lionel@gmail.com>
2019-04-23 16:02:21 +0200

    Edit project name

--- sbadmin/wsgi.py
+++ sbadmin/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for csd_dashboard project.
+WSGI config for sbadmin project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'csd_dashboard.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sbadmin.settings')
 
 application = get_wsgi_application()
