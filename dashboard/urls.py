4dcaf3863f316c1983bdaa9e25535ada291ac6f4
Nels885 <voirin.lionel@gmail.com>
2019-04-23 15:09:12 +0200

    Add templates for demo

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,5 +6,17 @@ app_name = 'dashboard'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('buttons/', views.buttons, name='buttons'),
+    path('cards/', views.cards, name='cards'),
+    path('colors/', views.colors, name='colors'),
+    path('border/', views.border, name='border'),
+    path('animation/', views.animation, name='animation'),
+    path('other/', views.other, name='other'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('password/', views.forgot_pwd, name='password'),
     path('blank/', views.blank, name='blank'),
+    path('error/', views.error, name='error'),
+    path('charts/', views.charts, name='charts'),
+    path('table/', views.tables, name='tables'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,14 +2,100 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
     context = {
         'title': 'Dashboard',
     }
     return render(request, 'dashboard/index.html', context)
 
+
+def buttons(request):
+    context = {
+        'title': 'Buttons',
+    }
+    return render(request, 'dashboard/buttons.html', context)
+
+
+def cards(request):
+    context = {
+        'title': 'Cards',
+    }
+    return render(request, 'dashboard/cards.html', context)
+
+
+def colors(request):
+    context = {
+        'title': 'Color Utilities',
+    }
+    return render(request, 'dashboard/colors.html', context)
+
+
+def border(request):
+    context = {
+        'title': 'Border Utilities',
+    }
+    return render(request, 'dashboard/border.html', context)
+
+
+def animation(request):
+    context = {
+        'title': 'Animation Utilities',
+    }
+    return render(request, 'dashboard/animation.html', context)
+
+
+def other(request):
+    context = {
+        'title': 'Other Utilities',
+    }
+    return render(request, 'dashboard/other.html', context)
+
+
+def login(request):
+    context = {
+        'title': 'Login',
+    }
+    return render(request, 'dashboard/login.html', context)
+
+
+def register(request):
+    context = {
+        'title': 'Register',
+    }
+    return render(request, 'dashboard/register.html', context)
+
+
+def forgot_pwd(request):
+    context = {
+        'title': 'Forgot Password',
+    }
+    return render(request, 'dashboard/forgot-password.html', context)
+
+
 def blank(request):
     context = {
         'title': 'Blank Page',
     }
     return render(request, 'dashboard/blank.html', context)
+
+
+def error(request):
+    context = {
+        'title': '404 Page',
+    }
+    return render(request, '404.html', context)
+
+
+def charts(request):
+    context = {
+        'title': 'Charts',
+    }
+    return render(request, 'dashboard/charts.html', context)
+
+
+def tables(request):
+    context = {
+        'title': 'Tables',
+    }
+    return render(request, 'dashboard/tables.html', context)
