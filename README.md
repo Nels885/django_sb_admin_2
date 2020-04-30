8a644b2ef1645d60bd69c4b9b287836dbea23b01
Nels885 <voirin.lionel@gmail.com>
2020-04-30 13:37:28 +0200

    Reorganization of templates and static files and update of Pipfile

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Django SB Admin 2
 
-This is an adaptation for Django 2.2 of SB Admin 2 theme, you can
+This is an adaptation for Django 3.x of SB Admin 2 theme, you can
 retrieve the original source of this theme at the address below:
 
 [https://github.com/BlackrockDigital/startbootstrap-sb-admin-2](https://github.com/BlackrockDigital/startbootstrap-sb-admin-2)
--- sbadmin/settings.py
+++ sbadmin/settings.py
@@ -31,7 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'dashboard.apps.DashboardConfig',
+    # My apps
+    'dashboard',
+
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -57,7 +60,9 @@ ROOT_URLCONF = 'sbadmin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,5 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # INTERNAL_IPS = '127.0.0.1'
